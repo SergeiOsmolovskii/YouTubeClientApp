f65d815582342723fe2586ba5afbde9255e81ed7
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-03-24 21:28:08 +0300

    feat: add search-result components

---
+++ YouTubeClientApp/src/app/search-result/video-info/video-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-info',
+  templateUrl: './video-info.component.html',
+  styleUrls: ['./video-info.component.scss']
+})
+export class VideoInfoComponent implements OnInit {
+
+/*   constructor() { } */
+
+  ngOnInit(): void {
+    console.log('VideoInfoComponent');
+  }
+
+}
