5af0836c6ceb74f5b4d532e8cc86a40141ab6f30
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-03-24 21:14:47 +0300

    feat: add header components

---
+++ YouTubeClientApp/src/app/header/sort-panel/sort-panel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-panel',
+  templateUrl: './sort-panel.component.html',
+  styleUrls: ['./sort-panel.component.scss']
+})
+export class SortPanelComponent implements OnInit {
+
+/*   constructor() { } */
+
+  ngOnInit(): void {
+    console.log('SortPanelComponent');
+  }
+
+}
