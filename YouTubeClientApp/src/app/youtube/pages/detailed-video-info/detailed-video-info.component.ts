0b5f29d7811401fb0107d52b4e38098ed3741b4c
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-04-06 22:51:29 +0300

    feat: add routing to template detailed video component

--- YouTubeClientApp/src/app/youtube/pages/detailed-video-info/detailed-video-info.component.ts
+++ YouTubeClientApp/src/app/youtube/pages/detailed-video-info/detailed-video-info.component.ts
@@ -1,17 +1,23 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IResponseItem } from '../../models/video-response.model';
 import { ActivatedRoute } from '@angular/router';
+import { VideoResponseService } from '../../services/video-response/video-response.service';
 
 @Component({
   selector: 'app-detailed-video-info',
   templateUrl: './detailed-video-info.component.html',
   styleUrls: ['./detailed-video-info.component.scss']
 })
-export class DetailedVideoInfoComponent /* implements OnInit */ {
-  @Input() videoInfo!: IResponseItem;
+export class DetailedVideoInfoComponent implements OnInit {
+  public videoInfo!: IResponseItem;
 
-  constructor(private routInfo: ActivatedRoute) { }
-/* 
+  constructor(private routInfo: ActivatedRoute, public videoResponse: VideoResponseService) { }
+
   ngOnInit(): void {
-  } */
+    this.videoInfo = this.getCurrentItem(this.routInfo.snapshot.params["id"]);
+  }
+
+  getCurrentItem(id: string): IResponseItem {
+    return this.videoResponse.response.find(item => item.id === id)!;
+  }
 }
