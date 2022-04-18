4039b8126ca8653a4b04a96a6db351a33e909a06
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-04-18 21:23:58 +0300

    fix: fix bug with detailed video info

--- YouTubeClientApp/src/app/youtube/pages/detailed-video-info/detailed-video-info.component.ts
+++ YouTubeClientApp/src/app/youtube/pages/detailed-video-info/detailed-video-info.component.ts
@@ -3,26 +3,29 @@ import { IResponseItem } from '../../models/video-response.model';
 import { ActivatedRoute } from '@angular/router';
 import { VideoResponseService } from '../../services/video-response/video-response.service';
 import { Location } from '@angular/common';
-
 @Component({
   selector: 'app-detailed-video-info',
   templateUrl: './detailed-video-info.component.html',
-  styleUrls: ['./detailed-video-info.component.scss']
+  styleUrls: ['./detailed-video-info.component.scss'],
 })
+
 export class DetailedVideoInfoComponent implements OnInit {
   public videoInfo!: IResponseItem;
 
   constructor(
-    private routInfo: ActivatedRoute, 
-    private videoResponse: VideoResponseService, 
-    private location: Location) { }
+    private routInfo: ActivatedRoute,
+    private videoResponse: VideoResponseService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
-    this.videoInfo = this.getCurrentItem(this.routInfo.snapshot.params["id"]);
-  }
-
-  getCurrentItem(id: string): IResponseItem {
-    return this.videoResponse.response.find(item => item.id.videoId === id)!;
+    if (this.routInfo.snapshot.params['id']) {
+      this.videoResponse
+        .getStatic([this.routInfo.snapshot.params['id']])
+        .subscribe((data) => {
+          this.videoInfo = data.items[0];
+        });
+    }
   }
 
   goBack(): void {
