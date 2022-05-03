2d87535b1222e8d55b1fd5cd5280ebd045541095
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2022-05-03 15:42:19 +0300

    feat: add ngrx settings

--- YouTubeClientApp/src/app/app.module.ts
+++ YouTubeClientApp/src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { YoutubeModule } from './youtube/youtube.module';
 import { AuthModule } from './auth/auth.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './redux/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './redux/reducers/effects/app.effects';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +23,12 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     CoreModule,
     YoutubeModule,
-    AuthModule
+    AuthModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
